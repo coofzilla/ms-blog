e91d6169251fabebfa43c0d422a4927bfbb83256
Jay Hernandez <jeric614@yahoo.com>
2021-10-17 12:18:30 +0900

    emitted event to event bus

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -12,16 +13,23 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
 
   const { title } = req.body;
-  console.log(title);
 
   posts[id] = {
     id,
     title,
   };
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id,
+      title,
+    },
+  });
 
   res.status(201).send(posts[id]);
 });
