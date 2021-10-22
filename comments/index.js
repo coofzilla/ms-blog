6f0bab87ccd0bdcfa289f6115f799c77f7469127
Jay Hernandez <jeric614@yahoo.com>
2021-10-22 10:21:06 +0900

    verified communication w/kube cluster

--- comments/index.js
+++ comments/index.js
@@ -37,6 +37,8 @@ app.post('/posts/:id/comments', async (req, res) => {
 });
 
 app.post('/events', async (req, res) => {
+  console.log("Event Received:", req.body.type);
+
   const { type, data } = req.body;
 
   if (type === 'CommentModerated') {
